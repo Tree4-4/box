56d790ff07287065f2dc7449581e54e9d62554c4
Tree4-4 <trey65ts@gmail.com>
2024-12-16 00:43:32 -0600

    not done

---
+++ js/app.js
@@ -0,0 +1,59 @@
+class Game {
+    constructor() {
+        this.countDisplay = document.getElementById('countDisplay')
+        this.gameBoard = document.getElementById('gameBoard')
+        this.count = 0
+    }
+}
+const colors = {
+    red, 
+    blue,
+    purple,
+    black,
+    white,
+    green,
+    brown,
+    pink,
+    yellow
+}
+
+obj = boxes = [
+    {
+        id: 1,
+        color: 'red'
+    },
+    {
+        id: 2,
+        color: 'blue'
+    },
+    {
+        id: 3,
+        color: 'purple'
+    },
+    {
+        id: 4,
+        color: 'black'
+    },
+    {
+        id: 5,
+        color: 'white'
+    },
+    {
+        id: 6,
+        color: 'green'
+    },
+    {
+        id: 7,
+        color: 'brown'
+    },
+    {
+        id: 8,
+        color: 'pink'
+    },
+    {
+        id: 9,
+        color: 'yellow'
+    }
+
+]
+
